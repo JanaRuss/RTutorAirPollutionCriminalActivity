eceea4f368dba30c339939a28ccf999de583a42a
JanaRuss <jana.russ@uni-ulm.de>
2023-07-23 15:41:44 +0200

    upload

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorAirPollCrime"
+  
+  # Name of problem sets in the package
+  ps = c("AirPollutionCriminalActivity")
+  
+  list(package=package, ps = ps)
+}
